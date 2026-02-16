efa4a08846e969b834a6bf55ce9dea024532eb63
dossec <dossec@gmail.com>
2026-02-16 09:52:30 +0800

    Uncomment git clone for small-package repository

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #git clone https://github.com/x-wrt/com.x-wrt.git package/com.x-wrt
-#git clone https://github.com/kenzok8/small-package package/small
+git clone https://github.com/kenzok8/small-package package/small
 #git clone https://github.com/xuanranran/openwrt-package.git package/xuanranran
 #git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/kiddin9/kwrt-packages package/kwrt
